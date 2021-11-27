e44488f8b90bf3971948c675b0f6ab18a4b89c88
Liudmila Antonova <liudm.antonova@gmail.com>
2021-11-27 10:47:02 +0300

    2nd homework

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-"use strict"
-let product = 'table';
-let price = 100;
-let message = `${product}, цена ${price} рублей`;
-alert(message);
+function RandGen (min, max) {
+    if (min >= max) {
+        return 'Минимум должен быть меньше  максимума';
+      } else {
+    return Math.round(Math.random() * (max - min) + min);
+    	}
+}
+
+console.log(RandGen(2,100));
+console.log(RandGen(100,2));
